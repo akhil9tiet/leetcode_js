f2da0d6617265b0b8d8e6902b3b611ae3f69c2d4
Akhil <akhil9tiet@gmail.com>
2020-10-01 11:58:52 -0700

    287: medium but very easy

---
+++ 14.longest-common-prefix.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=14 lang=javascript
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+
+  let minLen = strs.map(str => Math.min())
+    
+};
+// @lc code=end
+
